fddc9e53714af5084001e950232a3c725e83d1b2
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2023-07-30 13:56:17 -0400

    Do Problem Set 1 Mario less

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,5 +3,33 @@
 
 int main(void) 
 {
+    // Get height of pyramid from user.
+    int height;
+    const int MIN_HEIGHT = 1;
+    const int MAX_HEIGHT = 8;
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < MIN_HEIGHT || height > MAX_HEIGHT);
+
+    // Print a right-aligned pyramid.
+    int spaces = height - 1;
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < height; col++)
+        {
+            if (col < spaces)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        spaces--;
+        printf("\n");
+    }
     
+    return 0;
 }
