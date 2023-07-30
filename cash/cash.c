e23889dde8ccb9ff71c48e9a631637a688593a94
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2023-07-30 14:52:51 -0400

    Do Problem Set 1 Cash

--- cash/cash.c
+++ cash/cash.c
@@ -9,58 +9,67 @@ int calculate_pennies(int cents);
 
 int main(void)
 {
-    // Ask how many cents the customer is owed
+    // Ask how many cents the customer is owed.
     int cents = get_cents();
 
-    // Calculate the number of quarters to give the customer
+    // Calculate the number of quarters to give the customer.
     int quarters = calculate_quarters(cents);
-    cents = cents - quarters * 25;
+    cents -= quarters * 25;
 
-    // Calculate the number of dimes to give the customer
+    // Calculate the number of dimes to give the customer.
     int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
+    cents -= dimes * 10;
 
-    // Calculate the number of nickels to give the customer
+    // Calculate the number of nickels to give the customer.
     int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
+    cents -= nickels * 5;
 
-    // Calculate the number of pennies to give the customer
+    // Calculate the number of pennies to give the customer.
     int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
+    cents -= pennies * 1;
 
-    // Sum coins
+    // Sum coins.
     int coins = quarters + dimes + nickels + pennies;
 
-    // Print total number of coins to give the customer
-    printf("%i\n", coins);
+    // Print total number of coins to give the customer.
+    printf("%d\n", coins);
+
+    return 0;
 }
 
 int get_cents(void)
 {
-    // TODO
-    return 0;
+    int cents;
+    do
+    {
+        cents = get_int("Change owed: ");
+    } while (cents < 0);
+    
+    return cents;
 }
 
 int calculate_quarters(int cents)
 {
-    // TODO
-    return 0;
+    int quarters = cents / 25;
+
+    return quarters;
 }
 
 int calculate_dimes(int cents)
 {
-    // TODO
-    return 0;
+    int dimes = cents / 10;
+
+    return dimes;
 }
 
 int calculate_nickels(int cents)
 {
-    // TODO
-    return 0;
+    int nickels = cents / 5;
+
+    return nickels;
 }
 
 int calculate_pennies(int cents)
 {
-    // TODO
-    return 0;
+    return cents;
 }
