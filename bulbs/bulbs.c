62c124e8df708925a99ba0f34bb6ae43613f1a5e
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2023-08-03 17:01:17 -0400

    Do Problem Set 2 Bulbs

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -8,19 +8,48 @@ void print_bulb(int bit);
 
 int main(void)
 {
-    // TODO
+    string message = get_string("Message: ");
+    int length = strlen(message);
+
+    for (int i = 0; i < length; i++)
+    {
+        char c = message[i]; // A character of the message.
+        int ascii = c; // ASCII code of the character.
+        
+        // Binary number of the ASCII code.
+        int bits[BITS_IN_BYTE] = {0};
+        int current_index = BITS_IN_BYTE - 1;
+        while (ascii > 0)
+        {
+            if (ascii % 2 == 1)
+            {
+                bits[current_index] = 1;
+            }
+            ascii /= 2;
+            current_index--;
+        }
+        
+        for (int j = 0; j < BITS_IN_BYTE; j++)
+        {
+            print_bulb(bits[j]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
 }
 
 void print_bulb(int bit)
 {
     if (bit == 0)
     {
-        // Dark emoji
+        // Dark emoji.
         printf("\U000026AB");
     }
     else if (bit == 1)
     {
-        // Light emoji
+        // Light emoji.
         printf("\U0001F7E1");
     }
 }
