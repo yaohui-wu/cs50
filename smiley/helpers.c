c74592633df701723357501a21ee881905ed6274
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2023-08-08 21:59:15 -0400

    Do Lab 4 Smiley

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,5 +2,18 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to a color of your choosing
+    // Change all black pixels to a color of your choosing.
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            if (image[row][col].rgbtRed == 0
+                && image[row][col].rgbtGreen == 0
+                && image[row][col].rgbtBlue == 0)
+            {
+                // Change all black pixels to blue.
+                image[row][col].rgbtBlue = 255;
+            }
+        }
+    }
 }
