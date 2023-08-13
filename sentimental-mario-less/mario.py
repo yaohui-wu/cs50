7e907c48c17aebe1f8c0c97fc936371b8bcb1557
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2023-08-13 17:52:14 -0400

    Do Problem Set 6 Mario less

---
+++ sentimental-mario-less/mario.py
@@ -0,0 +1,27 @@
+from cs50 import get_int
+
+
+def main():
+    height = get_height()
+    print_pyramid(height)
+
+
+def get_height():
+    height = get_int('Height: ')
+
+    while height < 1 or height > 8:
+        height = get_int('Height: ')
+    
+    return height
+
+
+def print_pyramid(height):
+    spaces = height - 1
+
+    for i in range(1, height + 1):
+        print(' ' * spaces, end='')
+        print('#' * i)
+        spaces -= 1
+
+
+main()
